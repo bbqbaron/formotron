9ef47694d96dc7bc25fe3d88977d26b1592c45e2
Eric Loren <osiris1723@gmail.com>
2015-05-02 14:53:36 -0400

    uncomment code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var Form = require('./lib/model/Form'),
 	FormView = require('./lib/view/Form');
 
-// module.exports = {
-// 	Form: Form,
-// 	FormView: FormView
-// };
-
-module.exports = {};
+module.exports = {
+	Form: Form,
+	FormView: FormView
+};
